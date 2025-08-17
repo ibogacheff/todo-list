200d311da90a24a2c37f87d3e3c0c4e96bb13a6b
Dmitry Bogachev <db3666702@gmail.com>
2025-08-17 16:32:59 +0300

    refactor: create constants

--- src/main.js
+++ src/main.js
@@ -1,3 +1,12 @@
+const deletedState = "deleted";
+const TODO_TEXT = ".todo__text";
+const TODO_OPTIONS = ".todo__options";
+const TODO_UPDATEDAT = "todo__updatedAt";
+const TODO_ACTION = "todo__action";
+const TODO_ITEM = "todo__item";
+const TODO_DESCRIPTION = "todo__description";
+const TODO_ITEMS = ".todo__items";
+
 const optionsDate = {
   year: "numeric",
   month: "2-digit",
@@ -9,20 +18,19 @@ const optionsTime = {
   second: "2-digit",
 };
 
-const deletedState = "deleted";
-
 const todo = {
   action(e) {
     const target = e.target;
-    if (target.classList.contains("todo__action")) {
+    if (target.classList.contains(TODO_ACTION)) {
       const action = target.dataset.todoAction;
       const isDescribe = target.dataset.todoDescribe;
-      const elemItem = target.closest(".todo__item");
+      const elemItem = target.closest(`.${TODO_ITEM}`);
 
       if (isDescribe) {
-        const todoItemContainer = target.closest(".todo__item");
-        const descriptionElement =
-          todoItemContainer.querySelector(".todo__description");
+        const todoItemContainer = target.closest(`.${TODO_ITEM}`);
+        const descriptionElement = todoItemContainer.querySelector(
+          `.${TODO_DESCRIPTION}`
+        );
         this.addDescription(descriptionElement);
         return;
       }
@@ -34,8 +42,8 @@ const todo = {
         elemItem.remove();
       } else {
         elemItem.dataset.todoState = action;
-        if (elemItem.querySelector(".todo__updatedAt")) {
-          elemItem.querySelector(".todo__updatedAt").remove();
+        if (elemItem.querySelector(`.${TODO_UPDATEDAT}`)) {
+          elemItem.querySelector(`.${TODO_UPDATEDAT}`).remove();
         }
         this.addUpdatedAt(elemItem);
       }
@@ -49,25 +57,25 @@ const todo = {
     const date = new Date();
     const itemUpdationDate = date.toLocaleDateString("ru-RU", optionsDate);
     const itemUpdationTime = date.toLocaleTimeString("ru-RU", optionsTime);
-    const updatedAtElement = elemItem.querySelector(".todo__updatedAt");
+    const updatedAtElement = elemItem.querySelector(`.${TODO_UPDATEDAT}`);
 
     if (updatedAtElement) {
       updatedAtElement.textContent = `Изменена: ${itemUpdationDate} в ${itemUpdationTime}`;
     } else {
       elemItem.insertAdjacentHTML(
         "beforeend",
-        `<div class="todo__updatedAt">Изменена: ${itemUpdationDate} в ${itemUpdationTime}</div>`
+        `<div class=${TODO_UPDATEDAT}>Изменена: ${itemUpdationDate} в ${itemUpdationTime}</div>`
       );
     }
   },
   addNewTask() {
-    const todoTextElem = document.querySelector(".todo__text");
+    const todoTextElem = document.querySelector(TODO_TEXT);
 
     if (todoTextElem.disabled || !todoTextElem.value.length) {
       return;
     }
 
-    const todoItemsElem = document.querySelector(".todo__items");
+    const todoItemsElem = document.querySelector(TODO_ITEMS);
     const todoItemString = this.createTodoItemString(todoTextElem.value);
 
     todoItemsElem.insertAdjacentHTML("beforeend", todoItemString);
@@ -76,14 +84,14 @@ const todo = {
   createTodoItemString(titleTask) {
     const { itemCreationDate, itemCreationTime } = this.addCreatedAt();
 
-    return `<li class="todo__item" data-todo-state="active">
+    return `<li class=${TODO_ITEM} data-todo-state="active">
                     <span class="todo__task">${titleTask}</span>
-                    <div class="todo__description">Описание</div>
+                    <div class=${TODO_DESCRIPTION}>Описание</div>
                     <div class="todo__createdAt">Создана: ${itemCreationDate} в ${itemCreationTime}</div>
-                    <span class="todo__action todo__action_restore" data-todo-action="active"></span>
-                    <span class="todo__action todo__action_complete" data-todo-action="completed"></span>
-                    <span class="todo__action todo__action_delete" data-todo-action="deleted"></span>
-                    <span class="todo__action todo__action_describe" data-todo-action="active" data-todo-describe="true"></span>
+                    <span class="${TODO_ACTION} todo__action_restore" data-todo-action="active"></span>
+                    <span class="${TODO_ACTION} todo__action_complete" data-todo-action="completed"></span>
+                    <span class="${TODO_ACTION} todo__action_delete" data-todo-action="deleted"></span>
+                    <span class="${TODO_ACTION} todo__action_describe" data-todo-action="active" data-todo-describe="true"></span>
                 </li>`;
   },
   addCreatedAt() {
@@ -97,23 +105,20 @@ const todo = {
     const fromStorage = localStorage.getItem("todo");
 
     if (fromStorage) {
-      document.querySelector(".todo__items").innerHTML = fromStorage;
+      document.querySelector(TODO_ITEMS).innerHTML = fromStorage;
     }
     document
-      .querySelector(".todo__options")
+      .querySelector(TODO_OPTIONS)
       .addEventListener("change", this.filterTasks);
     document.addEventListener("click", this.action.bind(this));
   },
   filterTasks() {
-    const option = document.querySelector(".todo__options").value;
-    document.querySelector(".todo__items").dataset.todoOption = option;
-    document.querySelector(".todo__text").disabled = option !== "active";
+    const option = document.querySelector(TODO_OPTIONS).value;
+    document.querySelector(TODO_ITEMS).dataset.todoOption = option;
+    document.querySelector(TODO_TEXT).disabled = option !== "active";
   },
   saveTask() {
-    localStorage.setItem(
-      "todo",
-      document.querySelector(".todo__items").innerHTML
-    );
+    localStorage.setItem("todo", document.querySelector(TODO_ITEMS).innerHTML);
   },
   addDescription(descriptionElement) {
     let descriptionText = prompt("Пожалуйста, добавьте описание:");
@@ -124,7 +129,7 @@ const todo = {
 
     if (descriptionElement) {
       descriptionElement.textContent = descriptionText;
-      this.addUpdatedAt(descriptionElement.closest(".todo__item"));
+      this.addUpdatedAt(descriptionElement.closest(`.${TODO_ITEM}`));
     }
   },
 };
